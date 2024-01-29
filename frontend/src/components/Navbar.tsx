350e3bfcc2dd936775afc50080224e9115402a26
Nakii0603 <naakii0603@gmail.com>
2024-01-29 13:08:28 +0800

    add navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { PineSvg } from "./Svg";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+export const Navbar = () => {
+  return (
+    <div className="flex justify-between w-[1440px] mx-auto">
+      <div className="flex gap-[30px] items-center">
+        <PineSvg />
+        <p>НҮҮР</p>
+        <p className="text-[#18BA51]">ХООЛНЫ ЦЭС</p>
+        <p>ХҮРГЭЛТИЙН БҮС</p>
+      </div>
+      <div className="flex gap-[40px]">
+        <div className="flex border-2 border-grey">
+          <SearchIcon />
+          <input className="rounded-xl" type="text" placeholder="Хайх" />
+        </div>
+        <div className="flex gap-[40px]">
+          <div className="flex gap-3 items-center">
+            <ShoppingBasketIcon />
+            <p>Сагс</p>
+          </div>
+          <div className="flex gap-3 items-center">
+            <AccountCircleIcon />
+            <p>Нэвтрэх</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
