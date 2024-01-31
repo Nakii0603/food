eae303f858ce27b56ff3c3bc01cf70b33a38269c
Nakii0603 <naakii0603@gmail.com>
2024-01-31 09:41:12 +0800

    add sign

--- frontend/src/app/sign/page.tsx
+++ frontend/src/app/sign/page.tsx
@@ -1,15 +1,16 @@
 "use client";
-//import React from "react";
-import { useState } from "react";
+import React from "react";
 import axios from "axios";
-export default function signUp() {
+import { useState } from "react";
+
+export default function SignUp() {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
   const [password, setPassword] = useState("");
+
   const HandletoSignUp = async () => {
     try {
-      //const url = "http://localhost:8000/signUp";
       await axios.post("http://localhost:8000/signUp", {
         username: name,
         email: email,
@@ -17,43 +18,70 @@ export default function signUp() {
         password: password,
       });
     } catch (err) {
-      console.log(err);
+      console.log(err, "axios");
     }
   };
   return (
-    <div>
-      <input
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-        className="border-2"
-        type="text"
-        placeholder="Нэрээ оруулна уу"
-      />
-      <input
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-        className="border-2"
-        type="text"
-        placeholder="И-мэйл хаягаа оруулна уу"
-      />
-      <input
-        onChange={(e) => setAddress(e.target.value)}
-        value={address}
-        className="border-2"
-        type="text"
-        placeholder="Та хаягаа оруулна уу"
-      />
-      <input
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-        className="border-2"
-        type="text"
-        placeholder="Нууц үгээ оруулна уу"
-      />
-      <button
-        onClick={() => HandletoSignUp()}
-        className="w-10 h-20 bg-black"
-      ></button>
+    <div className="flex justify-center">
+      <div className="flex flex-col  p-[32px] gap-12 box-border">
+        <h1 className="text-[28px] text-center ">Бүртгүүлэх</h1>
+        <div className="flex flex-col gap-[16px]">
+          <div>
+            <p className="mb-1">Нэр</p>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              className="px-[16px] py-[8px] w-[350px] box-border border-[1px] rounded-[4px] text-[#8B8E95] border-[#8B8E95] bg-[#ECEDF0]"
+              type="text"
+              placeholder="Нэрээ оруулна уу"
+            />
+          </div>
+          <div>
+            <p className="mb-1">И-мэйл</p>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              className="px-[16px] py-[8px] w-[350px] box-border border-[1px] rounded-[4px] text-[#8B8E95] border-[#8B8E95] bg-[#ECEDF0]"
+              type="text"
+              placeholder="И-мэйл хаягаа оруулна уу"
+            />
+          </div>
+          <div>
+            <p className="mb-1">Утас</p>
+            <input
+              onChange={(e) => setAddress(e.target.value)}
+              value={address}
+              className="px-[16px] py-[8px] w-[350px] box-border border-[1px] rounded-[4px] text-[#8B8E95] border-[#8B8E95] bg-[#ECEDF0]"
+              type="text"
+              placeholder="Та утасны дугаар оруулна уу"
+            />
+          </div>
+          <div>
+            <p className="mb-1">Нууц үг</p>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              className="px-[16px] py-[8px] w-[350px] box-border border-[1px] rounded-[4px] text-[#8B8E95] border-[#8B8E95] bg-[#ECEDF0]"
+              type="text"
+              placeholder="Нууц үгээ оруулна уу"
+            />
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div>
+            <input className="" type="checkbox" />
+          </div>
+          <button className="px-[16px] py-[8px] w-[350px]">
+            Үйлчилгээний нөхцөл зөвшөөрөх
+          </button>
+        </div>
+        <button
+          onClick={HandletoSignUp}
+          className="px-[16px] py-[8px] w-[350px]  text-black bg-[#EEEFF2]"
+        >
+          Бүртгүүлэх
+        </button>
+      </div>
     </div>
   );
 }
